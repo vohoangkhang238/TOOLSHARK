fbd7ad3fefbd5049c4a822543ae0c75b673baecd
Hoang Khang <your_email@gmail.com>
2025-12-07 16:01:11 +0700

    Auto-sync: file changes

--- src/main/java/com/sojourners/chess/menu/BoardContextMenu.java
+++ src/main/java/com/sojourners/chess/menu/BoardContextMenu.java
@@ -13,50 +13,58 @@ public class BoardContextMenu extends ContextMenu {
 
     /** * 私有构造函数 */
     private BoardContextMenu() {
-        // Đã ẩn (comment) tính năng "编辑局面"
+        /* BẮT ĐẦU ẨN các tính năng theo yêu cầu (bao gồm 编辑局面) */
+        
+        // Ẩn: 编辑局面
         // MenuItem editMenuItem = new MenuItem("编辑局面");
         // getItems().add(editMenuItem);
         // getItems().add(new SeparatorMenuItem());
 
-        MenuItem copyFenMenuItem = new MenuItem("复制局面FEN");
-        MenuItem pasteFenMenuItem = new MenuItem("粘贴局面FEN");
-        getItems().addAll(copyFenMenuItem, pasteFenMenuItem);
-        getItems().add(new SeparatorMenuItem());
+        // Ẩn: 复制局面FEN & 粘贴局面FEN (đã ẩn từ trước)
+        // MenuItem copyFenMenuItem = new MenuItem("复制局面FEN");
+        // MenuItem pasteFenMenuItem = new MenuItem("粘贴局面FEN");
+        // getItems().addAll(copyFenMenuItem, pasteFenMenuItem);
+        // getItems().add(new SeparatorMenuItem());
 
-        MenuItem copyImageMenuItem = new MenuItem("复制局面图片");
-        MenuItem pasteImageMenuItem = new MenuItem("粘贴局面图片");
-        getItems().addAll(copyImageMenuItem, pasteImageMenuItem);
-        getItems().add(new SeparatorMenuItem());
+        // Ẩn: 复制局面图片 & 粘贴局面图片 (đã ẩn từ trước)
+        // MenuItem copyImageMenuItem = new MenuItem("复制局面图片");
+        // MenuItem pasteImageMenuItem = new MenuItem("粘贴局面图片");
+        // getItems().addAll(copyImageMenuItem, pasteImageMenuItem);
+        // getItems().add(new SeparatorMenuItem());
 
-        Menu timeMenu = new Menu("对局时间");
-        MenuItem timeOf01 = new MenuItem("0.1s");
-        MenuItem timeOf03 = new MenuItem("0.3s");
-        MenuItem timeOf05 = new MenuItem("0.5s");
-        MenuItem timeOf1 = new MenuItem("1s");
-        MenuItem timeOf2 = new MenuItem("2s");
-        MenuItem timeOf3 = new MenuItem("3s");
-        MenuItem timeOf5 = new MenuItem("5s");
-        MenuItem timeOf10 = new MenuItem("10s");
-        MenuItem timeOf15 = new MenuItem("15s");
-        timeMenu.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                String time = ((MenuItem) event.getTarget()).getText();
-                if (!"对局时间".equals(time)) {
-                    time = time.substring(0, time.length() - 1);
-                    long t = (long) (Double.parseDouble(time) * 1000);
-                    Properties prop = Properties.getInstance();
-                    prop.setAnalysisModel(Engine.AnalysisModel.FIXED_TIME);
-                    prop.setAnalysisValue(t);
-                }
-            }
-        });
-        timeMenu.getItems().addAll(timeOf01, timeOf03, timeOf05, timeOf1, timeOf2, timeOf3, timeOf5, timeOf10, timeOf15);
-        getItems().add(timeMenu);
-        getItems().add(new SeparatorMenuItem());
+        // Ẩn: 对局时间 (đã ẩn từ trước)
+        // Menu timeMenu = new Menu("对局时间");
+        // MenuItem timeOf01 = new MenuItem("0.1s");
+        // MenuItem timeOf03 = new MenuItem("0.3s");
+        // MenuItem timeOf05 = new MenuItem("0.5s");
+        // MenuItem timeOf1 = new MenuItem("1s");
+        // MenuItem timeOf2 = new MenuItem("2s");
+        // MenuItem timeOf3 = new MenuItem("3s");
+        // MenuItem timeOf5 = new MenuItem("5s");
+        // MenuItem timeOf10 = new MenuItem("10s");
+        // MenuItem timeOf15 = new MenuItem("15s");
+        // timeMenu.setOnAction(new EventHandler<ActionEvent>() {
+        //     @Override
+        //     public void handle(ActionEvent event) {
+        //         String time = ((MenuItem) event.getTarget()).getText();
+        //         if (!"对局时间".equals(time)) {
+        //             time = time.substring(0, time.length() - 1);
+        //             long t = (long) (Double.parseDouble(time) * 1000);
+        //             Properties prop = Properties.getInstance();
+        //             prop.setAnalysisModel(Engine.AnalysisModel.FIXED_TIME);
+        //             prop.setAnalysisValue(t);
+        //         }
+        //     }
+        // });
+        // timeMenu.getItems().addAll(timeOf01, timeOf03, timeOf05, timeOf1, timeOf2, timeOf3, timeOf5, timeOf10, timeOf15);
+        // getItems().add(timeMenu);
+        // getItems().add(new SeparatorMenuItem());
 
-        MenuItem switchMenuItem = new MenuItem("交换行棋方");
-        getItems().add(switchMenuItem);
+        // Ẩn: 交换行棋方 (đã ẩn từ trước)
+        // MenuItem switchMenuItem = new MenuItem("交换行棋方");
+        // getItems().add(switchMenuItem);
+        
+        /* KẾT THÚC ẨN các tính năng */
     }
 
     /** * 获取实例 * @return GlobalMenu */
